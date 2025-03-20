70e9c5bd81f86b5912049138a387a10e88c9b7c6
Sajal Jain <134300419+sajaljn26@users.noreply.github.com>
2025-03-20 09:50:00 +0530

    Time: 1 ms (32.79%), Space: 52.4 MB (31.7%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<nums.size();i++){
             if(i > farthest) return false;
             farthest = max(farthest,i + nums[i]);
-            if(i >= n-1){
+            if(i == n-1){
                 return true;
             }
         }
