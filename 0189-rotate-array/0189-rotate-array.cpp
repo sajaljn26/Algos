2382580fa0aa210d2755293b7509d5aa5c8bddfc
Sajal Jain <134300419+sajaljn26@users.noreply.github.com>
2025-12-08 21:54:39 +0530

    Time: 0 ms (100%), Space: 29.5 MB (60.14%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-      int n = nums.size();
-        k=k%n;
-        // in order to rotate the array we can use the reversal technique that we have found
+        // rotating the nums 
+        int n = nums.size();
+        k = k%n;
         reverse(nums.begin(),nums.end());
         reverse(nums.begin(),nums.begin() + k);
-        reverse(nums.begin()+k,nums.end());  
+        reverse(nums.begin() + k,nums.end());
     }
 };
