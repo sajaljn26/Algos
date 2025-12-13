2ac36f0e3eb5699bee0a9ae029f8fff7203f6ed3
Sajal Jain <134300419+sajaljn26@users.noreply.github.com>
2025-12-13 23:43:46 +0530

    Time: 0 ms (100%), Space: 12.5 MB (76.12%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,27 +5,21 @@ public:
         if(n==1){
             return 0;
         }
-        if(nums[0] > nums[1]){
-            return 0;
-        }
-        if(nums[n-1] > nums[n-2]){
-            return n-1;
-        }
-        int low = 1;
-        int high = n-2;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+        int low =1;
+        int high = n-1;
         while(high >= low){
-            int mid = (low+high)/2;
-            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1] ){
+            int mid = (low + high)/2;
+            if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]){
                 return mid;
             }
-            if(nums[mid] < nums[mid+1]){
-                     low = mid+1;
+            if(nums[mid] > nums[mid-1]){
+                low = mid +1;
             }else{
-                   high = mid-1;
+                high = mid-1;
             }
         }
         return -1;
-        
-    }
-
+     }
 };
