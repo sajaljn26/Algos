922649cc84fa8599ecdd6ed159ff16f5e58e3e7b
Sajal Jain <134300419+sajaljn26@users.noreply.github.com>
2025-12-15 20:12:59 +0530

    Time: 0 ms (100%), Space: 9.8 MB (25.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s == t;
+        if(s.size() != t.size()){
+            return false;
+        }
+        int fre[26] = {0};
+        for(char c : s){
+            fre[c - 'a']++;
+        }
+        for(char n : t){
+            fre[n - 'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(fre[i] != 0){
+                return false;
+            }
+        }
+        return true;
+
     }
 };
